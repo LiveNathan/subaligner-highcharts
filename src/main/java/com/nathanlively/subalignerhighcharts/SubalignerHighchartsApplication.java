5e6875c45e9c7e4fb13539e2f18781a8f60d4f2e
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-12-27 10:50:26 -0600

    first commit

---
+++ src/main/java/com/nathanlively/subalignerhighcharts/SubalignerHighchartsApplication.java
@@ -0,0 +1,13 @@
+package com.nathanlively.subalignerhighcharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubalignerHighchartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubalignerHighchartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathanlively/subalignerhighcharts/SubalignerHighchartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanlively.subalignerhighcharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubalignerHighchartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
